25f7e55131e60600a114fbce86358e255138eceb
Mikhail Navitski <navitski.mikhail23@gmail.com>
2024-10-09 14:17:31 +0300

    feat: add loader refactor: rename vars for better readability

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 
 let apiQuotes = [];
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //Show New Quote
 function newQuote() {
+    showLoadingSpinner()
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes.quotes[Math.floor(Math.random() * apiQuotes.quotes.length)]
     authorText.textContent = quote.author;
@@ -18,11 +30,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    // Set Quote, Hide loader
     quoteText.textContent = quote.quote;
+    removeLoadingSpinner()
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    showLoadingSpinner()
     const apiUrl = 'https://dummyjson.com/quotes';
     try {
         const response = await fetch(apiUrl);
